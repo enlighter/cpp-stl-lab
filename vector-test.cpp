ab9f1d6e68c9195f8414a77044e6185734c373c1
enlighter <mandal.sushovan92@gmail.com>
2016-02-26 09:38:22 +0530

    successfull implementation of dynamic removal

--- vector-test.cpp
+++ vector-test.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
 	vector<int> i = {2,3,4,5,6};
+	int pad = i.back();
 
 	for(auto it = i.begin(); it!=i.end(); ++it)
 	{
 		if(*it > 3)
 		{
 			*it = i.back();
-			i.pop_back();
 		}
 		/*if(*it == 5)
 		{
 			i.erase(it);
 		}*/
+	}
+
+	while(std::find(i.begin(),i.end(),pad) != i.end())
+	{
+		i.erase(std::find(i.begin(),i.end(),pad));
 	}
 
 	for(auto it = i.begin(); it!=i.end(); ++it)
